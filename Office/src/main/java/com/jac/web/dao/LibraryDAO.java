584babe470c598bd88dbdb07d5e9352503f4d1e1
Roberto Di Biase <Roberto Di Biase>
2018-07-04 18:28:11 -0400

    Created getAllBooks method

--- Office/src/main/java/com/jac/web/dao/LibraryDAO.java
+++ Office/src/main/java/com/jac/web/dao/LibraryDAO.java
@@ -1,5 +1,40 @@
 package com.jac.web.dao;
 
-public class LibraryDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
+import com.jac.web.model.Book;
+
+public class LibraryDAO {
+	
+	public ArrayList<Book> getAllBooks() {
+		ArrayList<Book> books = new ArrayList<Book>();
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://den1.mysql3.gear.host:3306/javabeansproject",
+					"javabeansproject", "Ha6R4_U~2Gx0");
+			String query = "SELECT * from libray"; //NOT SURE ABOUT THE TABLE NAME
+			PreparedStatement st = con.prepareStatement(query);
+			ResultSet rs = st.executeQuery();
+
+			if (rs.next()) {
+				Book book = new Book();
+				int idFromDB = rs.getInt("bookID");
+				String nameFromDB = rs.getString("bookName");
+				String priceFromDB = rs.getString("authorName");
+				
+				book.setId(idFromDB);
+				book.setBookName(nameFromDB);
+				book.setAuthorName(priceFromDB);
+
+				books.add(book);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return books;
+	}
 }
--- Office/src/main/java/com/jac/web/model/Book.java
+++ Office/src/main/java/com/jac/web/model/Book.java
@@ -1,6 +1,6 @@
 package com.jac.web.model;
 
-public class Library {
+public class Book {
 	
 	private int id;
 	private String bookName;
