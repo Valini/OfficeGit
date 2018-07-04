fd0ad55e4204596dd43f27736519f4350f15eae6
MSI\Valini <valinipatten@yahoo.com>
2018-07-04 17:59:23 -0400

    Username 505

--- Office/src/main/java/com/jac/web/controller/LoginController.java
+++ Office/src/main/java/com/jac/web/controller/LoginController.java
@@ -28,6 +28,13 @@ public class LoginController extends HttpServlet {
 		// call method get Employee from DAO to check if this employee exists in
 		// database
 		Employee e1 = employee.getEmployee(employeeName);
+		//if no such employee name exists in the database, return to index page
+		if(e1.equals(null)) {
+			request.setAttribute("error", "Wrong username or password.");
+			RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+			rd.forward(request, response);	
+		}
+		
 		// check if password entered by employee matches password in database
 		if (password.equals(e1.getPassword())) {
 			request.setAttribute("employeeName", employeeName);
